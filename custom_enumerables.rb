273bf15be12c9775cfcdb3e68c4c10bf8ca92040
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-09-30 21:42:23 -0700

    added .all? custom enumerables

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -18,4 +18,31 @@ module Enumerable
             i +=1
         end
     end
+   
+    def my_select
+        items = self
+        i = 0
+        result = []
+        while i < items.length
+         if (yield(items[i]) == true)
+            result.push(items[i])
+         end
+            i += 1
+        end
+       return result
+    end
+
+    def my_all?(arg)
+        arr = self
+        puts arg
+        case arg
+            
+        in ^arr, *rest 
+           true
+        else
+           false
+        end
+    end
+
+    def my_any?(arg)
 end 
--- run_enumerables.rb
+++ run_enumerables.rb
@@ -1,9 +1,16 @@
 require_relative 'custom_enumerables'
 require 'pry-byebug'
 
-puts "my_each vs. each"
-numbers = [1, 2, 3, 4, 5]
+
+p numbers = [1,2,3,4,5]
+binding.pry
 #numbers.my_each { |item| puts item + 5}
 #numbers.each { |item| puts item}
-numbers.my_each_with_index { |item, index| puts " #{item} and #{index}"}
-numbers.each_with_index {|item, index| puts "#{item} and #{index}" } 
+#numbers.my_each_with_index { |item, index| puts " #{item} and #{index}"}
+#numbers.each_with_index {|item, index| puts "#{item} and #{index}" } 
+#puts result_mine = numbers.my_select { |num| num % 3 == 0}
+#puts result = numbers.select { |num| num % 2 == 0 }
+#numbers.my_all?(1)
+#numbers.all?(1)
+p numbers.my_any?(1)
+p numbers.any?(1)
